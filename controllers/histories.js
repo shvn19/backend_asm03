ba52e3b897b6a8a0bc15b4c253cd0afd26a531c3
shvn19 <shvn19@gmail.com>
2022-12-10 03:28:27 +0700

    Main history api

--- controllers/histories.js
+++ controllers/histories.js
@@ -1,5 +1,4 @@
 const Order = require("../models/order");
-const product = require("../models/product");
 
 
 exports.getUserHistory = (req, res, next) => {
@@ -8,17 +7,26 @@ exports.getUserHistory = (req, res, next) => {
   Order.find({ userId: idUser })
   .populate('products.product')
   .then(orders => {
+    const ordersToReturn = [];
     orders.forEach(o => {
-      // let t = 0;
-      // o.products.forEach((product,index) => {
-      //   o.populate(`products.product`);
+      // tính tổng lưu vào biến t
+      let t = 0;
+      o.products.forEach((orderDetail,index) => {
+        // o.populate(`products.product`);
         // o.index.populate({
         //   path: 'product', 
         //   model: 'Product'
         // });
+        t += orderDetail.product.price * orderDetail.count;
         console.log('#107 order: ', o.products[0].product);
-      // })
+      });
+      const tempOrder = o.toObject();
+      tempOrder.total = t;
+      console.log('#109 order after adding total: ', tempOrder);
+      ordersToReturn.push(tempOrder);
     });
+    console.log('#108 orders: ', ordersToReturn);
+    return res.status(200).json(ordersToReturn);
   })
   .catch(err => {
     console.log('#106 Lỗi trong quá trình tìm order: ', err);
